5d0789d98899ae933ddb6498c242dc6033e58c3e
inthemonzon <inthemonzon@yahoo.com>
2021-08-23 21:18:17 -0400

    commit tree 8ebd15e1d0c12965dc45fd10d12c20db16c63df2

--- app/models/network.rb
+++ app/models/network.rb
@@ -1,3 +1,10 @@
 class Network < ActiveRecord::Base
   
+has_many :shows
+has_many :characters, through: :shows
+has_many :actors, through: :characters
+
+  def sorry
+    "We're sorry about passing on John Mullaney's pilot"
+  end
 end
